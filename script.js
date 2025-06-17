ae7a8b1f79d5a2d656168ce355752133289d4bf1
Bubsha <106462568+Bubsha@users.noreply.github.com>
2025-06-17 17:55:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 const text = "Digital Marketing Specialist & Content Creator.";
 let index = 0;
 function type() {
-    document.getElementById("typing").textContent = text.slice(0, index++);
-    if (index <= text.length) {
-        setTimeout(type, 80);
-    }
+  document.getElementById("typing").textContent = text.slice(0, index++);
+  if (index <= text.length) {
+    setTimeout(type, 80);
+  }
 }
 type();
 
-// Hire Me Button Click Action
+// Hire Me Button Click
 document.getElementById("hireBtn").addEventListener("click", () => {
-    alert("📞 Call me at +91 9994473997");
+  alert("📞 Call me at +91 9994473997");
 });
